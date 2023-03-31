010c53e97bee2a7a8c6fe8fc34b7e7a359769955
omobolaji <abdjinadquadri@gmail.com>
2023-03-31 09:05:33 +0100

    Solved bugs

--- app.js
+++ app.js
@@ -1,88 +1,89 @@
-const number = document.querySelectorAll('.number');
-const pages = document.querySelectorAll('.page');
-const buttons = document.querySelectorAll('.butn');
-
-// for (i of btn) {
-//     i.addEventListener('click', pressMe);
-// }
-
-// function pressMe() {
-//     console.log('pressed');
-// }
-
+const number = document.querySelectorAll(".number");
+const pages = document.querySelectorAll(".page");
+const buttons = document.querySelectorAll(".butn");
 let currentPage = 1;
 
-const updatePage = (e) => {
-    currentPage = e.target.id === 'next' ? ++currentPage : --currentPage;
-    // console.log(currentPage);
+function navigatePage(newPage) {
+  // hide all pages
+  pages.forEach((page) => {
+    page.style.display = "none";
+  });
 
-    pages.forEach((page, index) => {
-        page.classList[`${index < currentPage ? 'add' : 'remove'}`]('');
-    });
-};
+  pages[newPage - 1].style.display = "block";
+  currentPage = newPage;
 
-buttons.forEach(butn => {
-    butn.addEventListener('click', updatePage);
+  updatePage();
+}
+
+function updatePage() {
+  pages.forEach((page, index) => {
+    page.classList[index < currentPage ? "add" : "remove"]("active");
+  });
+
+  // show or hide prevBtn and nextBtn based on current page number
+  const prevBtn = document.querySelector(`#prev-${currentPage}`);
+  const nextBtn = document.querySelector(`#next-${currentPage}`);
+
+  if (prevBtn) {
+    prevBtn.style.display = currentPage === 1 ? "none" : "block";
+  }
+
+  if (nextBtn) {
+    nextBtn.style.display = currentPage === pages.length ? "none" : "block";
+  }
+}
+
+function nextPage() {
+  if (currentPage === 4) {
+    navigatePage(5);
+  } else {
+    const steps = [stepOne, stepTwo, stepThree, stepFour];
+    const index = currentPage - 1;
+
+    if (index < steps.length) {
+      steps[index].click();
+    }
+
+    currentPage += 1;
+
+    navigatePage(currentPage);
+  }
+}
+
+function prevPage() {
+  currentPage -= 1;
+
+  navigatePage(currentPage);
+}
+
+const stepOne = document.querySelector(".number1"),
+  stepTwo = document.querySelector(".number2"),
+  stepThree = document.querySelector(".number3"),
+  stepFour = document.querySelector(".number4"),
+  pageFive = document.getElementById("page-5"),
+  confirmBtn = document.querySelector(".confirm");
+
+stepOne.addEventListener("click", () => {
+  navigatePage(1);
+});
+stepTwo.addEventListener("click", () => {
+  navigatePage(2);
+});
+stepThree.addEventListener("click", () => {
+  navigatePage(3);
+});
+stepFour.addEventListener("click", () => {
+  navigatePage(4);
+});
+confirmBtn.addEventListener("click", () => {
+  navigatePage(5);
 });
 
-const stepOne = document.querySelector('.number1'),
-      stepTwo = document.querySelector('.number2'),
-      stepThree = document.querySelector('.number3'),
-      stepFour = document.querySelector('.number4'),
-      pageFive = document.getElementById('page-5'),
-      nextBtn = document.querySelector('.btn-2'),
-      backBtn = document.querySelector('btn-1'),
-      confirmBtn = document.querySelector('.confirm');
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const page = parseInt(button.dataset.page, 10);
+    navigatePage(page);
+  });
+});
 
-stepOne.addEventListener('click', oneStep);
-stepTwo.addEventListener('click', twoStep);
-stepThree.addEventListener('click', threeStep);
-stepFour.addEventListener('click', fourStep);
-nextBtn.addEventListener('click', goNext);
-backBtn.addEventListener('click', goBack);
-confirmBtn.addEventListener('click', pageConfirm);
-
-function oneStep() {
-    document.getElementById('page-1').style.display = 'block';
-    document.getElementById('page-2').style.display = 'none';
-    document.getElementById('page-3').style.display = 'none';
-    document.getElementById('page-4').style.display = 'none';
-    stepOne.classList.add('active');
-    stepTwo.classList.remove('active');
-    stepThree.classList.remove('active');
-    stepFour.classList.remove('active');
-}
-
-function twoStep() {
-    document.getElementById('page-1').style.display = 'none';
-    document.getElementById('page-2').style.display = 'block';
-    document.getElementById('page-3').style.display = 'none';
-    document.getElementById('page-4').style.display = 'none';
-    stepTwo.classList.add('active');
-    stepOne.classList.remove('active');
-    stepThree.classList.remove('active');
-    stepFour.classList.remove('active');
-}
-
-function threeStep() {
-    document.getElementById('page-1').style.display = 'none';
-    document.getElementById('page-2').style.display = 'none';
-    document.getElementById('page-3').style.display = 'block';
-    document.getElementById('page-4').style.display = 'none';
-    stepThree.classList.add('active');
-    stepOne.classList.remove('active');
-    stepTwo.classList.remove('active');
-    stepFour.classList.remove('active');
-}
-
-function fourStep() {
-    document.getElementById('page-1').style.display = 'none';
-    document.getElementById('page-2').style.display = 'none';
-    document.getElementById('page-3').style.display = 'none';
-    document.getElementById('page-4').style.display = 'block';
-    stepFour.classList.add('active');
-    stepOne.classList.remove('active');
-    stepTwo.classList.remove('active');
-    stepThree.classList.remove('active');
-}   
-
+updatePage();
